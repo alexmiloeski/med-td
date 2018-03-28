247a98ee952c8f0c5d958acf2dd9d27117caa0b4
alexmilo <a.miloeski@gmail.com>
2018-03-28 19:46:46 +0200

    added basic tile generator
    -copied some default objects from another project;
    -script that generates a grid of tiles (of certain width and height, using a certain tile GameObject)
    -minor fix on .gitignore

---
+++ MedTD/Assets/Scripts/GenerateTiles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GenerateTiles : MonoBehaviour
+{
+    public int boardSizeX;
+    public int boardSizeY;
+    public Transform tilePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Renderer rend = tilePrefab.GetComponent<Renderer>();
+        Vector3 size = rend.bounds.size;
+        float width = size.x;
+        float height = size.y;
+        Debug.Log(width + " x " + height);
+
+        int halfBoardWidth = boardSizeX / 2;
+        int halfBoardHeight = boardSizeY / 2;
+
+        GameObject board = new GameObject();
+        board.name = "Board";
+        for (int x = -halfBoardWidth; x < halfBoardWidth; x++)
+        {
+            for (int y = -halfBoardHeight; y < halfBoardHeight; y++)
+            {
+                Transform tile = (Transform)Instantiate(tilePrefab, new Vector3(x*width, y*height, 0), Quaternion.identity);
+                tile.name = "Tile_" + (x+halfBoardWidth) + "_" + (y + halfBoardHeight);
+                tile.parent = board.transform;
+            }
+        }
+    }
+}
