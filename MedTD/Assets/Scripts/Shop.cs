6af70db29b898e3f7804da58ac46ca27c19ead24
alexmilo <a.miloeski@gmail.com>
2018-03-30 23:41:05 +0200

    Reordering Shop

--- MedTD/Assets/Scripts/Shop.cs
+++ MedTD/Assets/Scripts/Shop.cs
@@ -2,7 +2,6 @@
 
 public class Shop : MonoBehaviour
 {
-    public LymphNode lymphNode;
     public TowerBlueprint tower1;
     //public TowerBlueprint tower2;
 
@@ -14,27 +13,11 @@ public class Shop : MonoBehaviour
     {
         buildManager = BuildManager.instance;
     }
-
-    //public void BuySSButtonClick()
-    //{
-    //    Debug.Log("BuySSButtonClick");
-
-    //    if (somethingSelected)
-    //    {
-    //        buildManager.Deselect();
-    //    }
-    //    else
-    //    {
-    //        buildManager.StartSSSelection();
-    //    }
-
-    //    somethingSelected = !somethingSelected;
-    //}
-
+    
     public void BuildTower1()
     {
-        Debug.Log("clicked build tower 1");
-        //buildManager.BuildTower(tower1);
+        Debug.Log("Shop.BuildTower1");
+        buildManager.BuildTower1();
     }
 
     public void SellTower()
@@ -52,4 +35,43 @@ public class Shop : MonoBehaviour
     //    Debug.Log("clicked build tower 2");
     //    buildManager.SelectTurretToBuild(tower2);
     //}
+
+
+
+    // make sure button actions aren't called when dragging by checking if Scroller.IsDragging
+    public void ButtonDoneWithSSAction()
+    {
+        Debug.Log("Button done with ss");
+
+        if (!Scroller.instance.IsDragging())
+            BuildManager.instance.FinishWithSS();
+    }
+    public void ButtonBuildTower1Action()
+    {
+        Debug.Log("Button build tower 1");
+
+        if (!Scroller.instance.IsDragging())
+            BuildManager.instance.BuildTower1();
+    }
+    public void ButtonBuildTower2Action()
+    {
+        Debug.Log("Button build tower 2");
+
+        if (!Scroller.instance.IsDragging())
+            BuildManager.instance.BuildTower2();
+    }
+    public void ButtonBuildTower3Action()
+    {
+        Debug.Log("Button build tower 3");
+
+        if (!Scroller.instance.IsDragging())
+            BuildManager.instance.BuildTower3();
+    }
+    public void ButtonBuildTower4Action()
+    {
+        Debug.Log("Button build tower 4");
+
+        if (!Scroller.instance.IsDragging())
+            BuildManager.instance.BuildTower4();
+    }
 }
